b660fd493323224ab74576cf2105be6098e05890
Phil Huddy <pdh46@picotte001.cm.cluster>
2022-05-19 04:03:26 -0400

    finalizing tutorial code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 ECES 450-650 Tutorial 10
 
+This is the public repo for Tutorial 10 of ECES 450/650 at Drexel University
+
+The tutorial assumes that you have access to Drexel's high performance computing cluster: Picotte
+
+All of the data is available in the directory /ifs/groups/eces450650Grp/ECES450650_SP22/diamond/
+You do not need to run generate_data.sh, but it is there for reference for if you can not use picotte
+
+makedb.sh generates the diamond database that is required to run diamond with
+the database is made up of all uniprotkb proteins in from both swiss-prot and trembl
+
+tutorial.sh aligns the DNA query sequences against the diamond database
+tutorial_mp.sh is a script that will run diamond while taking advantage of distributed computing
--- tutorial_mp.sh
+++ tutorial_mp.sh
@@ -9,7 +9,7 @@
 ### select number of tasks per node
 #SBATCH --ntasks=1
 ### select number of cpus per task (you need to tweak this when you run a multi- thread program)
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=32
 ### request 15 minutes of wall clock time (if you request less time, you can wait for less time to get your job run by the system, you need to have a good estimation of the run time though).
 #SBATCH --time=06:00:00
 ### memory size required per node (this is important, you also need to estimate a upper bound)
@@ -21,18 +21,27 @@ module load diamond/2.0.15
 
 DATA_PATH=/ifs/groups/eces450650Grp/ECES450650_SP22/diamond
 DB_NAME=uniprot_diamond
-RESULTS_FILE=$DATA_PATH/uniprot_results/results_mid_sens
+RESULTS_FILE=$DATA_PATH/uniprot_results/results_ultra_sensitive
+
+diamond blastx -q $DATA_PATH/evol.fastq \
+	       -d $DATA_PATH/$DB_NAME \
+	       --multiprocessing \
+	       --mp-init \
+	       -t $DATA_PATH \
+	       --parallel-tmpdir $BEEGFS_TMPDIR
 
 # run blastx
 diamond blastx -q $DATA_PATH/evol.fastq \
                -d $DATA_PATH/$DB_NAME \
 	       -f 102 \
 	       -o $RESULTS_FILE \
-	       --mid-sensitive \
-	       -t $DATA_PATH
+	       --ultra-sensitive \
+	       --multiprocessing \
+	       -t $DATA_PATH \
+	       --parallel-tmpdir $BEEGFS_TMPDIR
 
 NUM_UNCLASS=$(cut -f 2 $RESULTS_FILE | grep ^0$ | wc -l)
 NUM_TOT=$(cat $RESULTS_FILE | wc -l)
 
-echo "Mid-Sensitive mode: $(($NUM_TOT-$NUM_UNCLASS))/$NUM_TOT classified"
+echo "Ultra-Sensitive mode: $(($NUM_TOT-$NUM_UNCLASS))/$NUM_TOT classified"
 
