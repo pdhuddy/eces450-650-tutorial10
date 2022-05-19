3e923db39b0a15aa4e90e8073296258be2528c7f
Phil Huddy <pdh46@picotte001.cm.cluster>
2022-05-18 22:32:19 -0400

    bash script for getting data to picotte and running diamond

--- generate_data.sh
+++ generate_data.sh
@@ -11,26 +11,41 @@
 ### select number of cpus per task (you need to tweak this when you run a multi- thread program)
 #SBATCH --cpus-per-task=1
 ### request 15 minutes of wall clock time (if you request less time, you can wait for less time to get your job run by the system, you need to have a good estimation of the run time though).
-#SBATCH --time=06:00:00
+#SBATCH --time=10:00:00
 ### memory size required per node (this is important, you also need to estimate a upper bound)
 #SBATCH --mem=16GB
 ### select the partition "def" (this is the default partition but you can change according to your application)
 #SBATCH --partition=def
 
-module load diamond
-
 DATA_PATH=/ifs/groups/eces450650Grp/ECES450650_SP22/diamond
 
-# create the diamond database if it doesnt exist already
-if [ ! -f "$DATA_PATH/reference.dmnd" ]
-then
-	echo "Database does not exist. Creating it"
-	diamond makedb --in $DATA_PATH/data.fasta -d $DATA_PATH/reference --taxonmap $DATA_PATH/prot.accession2taxid.FULL.gz --taxonnodes $DATA_PATH/nodes.dmp --taxonnames $DATA_PATH/names.dmp
-else
-	echo "Database exists. Continuing"
-fi
+#gzip -dk $DATA_PATH/gene_refseq_uniprotkb_collab.gz
+#sed '1d' $DATA_PATH/gene_refseq_uniprotkb_collab > $DATA_PATH/all_uniprot
+#cut -f2 $DATA_PATH/all_uniprot > $DATA_PATH/uniprot_ids
+#touch $DATA_PATH/data.fasta
 
-# run blastx
-diamond blastx -q /ifs/groups/eces450650Grp/data/mappings/evol1.sorted.unmapped.R1.fastq -d $DATA_PATH/reference -f 102 -o $DATA_PATH/results/results1_ultra_sens --ultra-sensitive -t $DATA_PATH
-diamond blastx -q /ifs/groups/eces450650Grp/data/mappings/evol1.sorted.unmapped.R2.fastq -d $DATA_PATH/reference -f 102 -o $DATA_PATH/results/results2_ultra_sens --ultra-sensitive -t $DATA_PATH
+#for ID in $(cat $DATA_PATH/uniprot_ids)
+#do
+#	wget https://www.uniprot.org/uniprot/$ID.fasta
+#	cat $ID.fasta >> $DATA_PATH/data.fasta
+#	rm $ID.fasta
+#done
+
+#rm $DATA_PATH/gene_refseq_uniprotkb_collab
+#rm $DATA_PATH/all_uniprot
+#rm $DATA_PATH/uniprot_ids
+
+CUR_DIR=$(pwd)
+
+cd $DATA_PATH
+
+wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
+wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz
+
+cd $CUR_DIR
+
+cat $DATA_PATH/uniprot_sprot.fasta.gz $DATA_PATH/uniprot_trembl.fasta.gz > $DATA_PATH/uniprot.fasta.gz
+
+#rm $DATA_PATH/uniprot_sprot.fasta.gz
+#rm $DATA_PATH/uniprot_trembl.fasta.gz
 
